112651e63e23758330f81f6dec9cd68047b97eb9
neilyboy <neilyboy@users.noreply.github.com>
2026-03-01 14:36:14 -0600

    fix: use __dirname and HOME env for ecosystem.config.js portability

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,16 @@
+const path = require('path');
+
 module.exports = {
   apps: [
     {
       name: 'pisign',
       script: 'node_modules/.bin/next',
       args: 'start -p 3000',
-      cwd: '/home/pi/rasppizero2w-signage-local',
+      cwd: __dirname,
       env: {
         NODE_ENV: 'production',
         DISPLAY: ':0',
-        XAUTHORITY: '/home/pi/.Xauthority',
+        XAUTHORITY: path.join(process.env.HOME || '/home/pi', '.Xauthority'),
       },
     },
   ],
